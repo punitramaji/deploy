429aeb1138a9c611ce75ec6476d7f8e71fdc2e94
Punit Ramaji <punitramaji@users.noreply.github.com>
2020-07-15 19:01:03 +0530

    Update demo-java.sh

--- demo-java.sh
+++ demo-java.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git clone https://github.com/tongueroo/demo-java.git
-mkdir -p ~/demo-java/pkg
-cd ~/demo-java/
+mkdir -p ~/deploy/demo-java/pkg
+cd ~/deploy/demo-java/
 mvn package
-cp ~/demo-java/target/demo.war ~/demo-java/pkg/demo.war
+cp ~/deploy/demo-java/target/demo.war ~/deploy/demo-java/pkg/demo.war
 docker build -t webapp .
 docker run --rm -p 8080:8080 -d webapp
-rm -rf ~/demo-java/pkg
+rm -rf ~/deploy/demo-java/pkg
 echo done
