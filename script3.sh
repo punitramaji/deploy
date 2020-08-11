a11f7ed0162c3b422200356b13627a7db9907832
root <root@ip-172-31-50-72.ec2.internal>
2020-08-11 18:40:43 +0000

    0 usage

---
+++ script3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pwd
+if [ $? -eq 0 ]
+then
+echo "Last executed command is successful"
+else
+echo "Last executed commnad is not successful"
+fi
+
+pwdd
+if [ $? -eq 0 ]
+then
+echo "Last executed command is successful"
+else
+echo "Last executed commnad is not successful"
+fi
